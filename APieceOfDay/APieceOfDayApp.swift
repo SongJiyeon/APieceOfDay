f696d2505f91328aec0f002429a5813741c4bdea
Songjiyeon <carol.jiyeon.song@gmail.com>
2022-07-16 13:34:53 +0900

    Initial Commit

---
+++ APieceOfDay/APieceOfDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  APieceOfDayApp.swift
+//  APieceOfDay
+//
+//  Created by 송지연 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct APieceOfDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APieceOfDay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APieceOfDay
+//
+//  Created by 송지연 on 2022/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
